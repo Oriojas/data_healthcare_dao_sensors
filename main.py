31cda61a8649b5fd1c40a4288aa0ba2f5ecb0284
Oriojas <oscarriojas@gmail.com>
2023-01-25 22:10:20 -0500

    feat: include sensors code and test fasAPI install

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import uvicorn
 from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/send_data/")
+async def send_data(user: str, bpm: float, spo2: int):
+    print("Hello FileCoin")
+    print(f"{user}, {bpm}, {spo2}")
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8088)
