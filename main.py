8fb63eb997ee423a13e9902a1cf71c7e859a5347
Oriojas <oscarriojas@gmail.com>
2023-01-29 18:26:55 -0500

    feat: get_wallet function

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pyodbc
 import uvicorn
 import subprocess
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -20,16 +22,29 @@ async def send_data(user: str, bpm: float, spo2: int):
 @app.get("/import_wallet/")
 async def import_wallet(token: str):
     if token == TOKEN:
-        wallet = subprocess.run(["lighthouse-web3", "import-wallet", "--key", f"{PK}"],
-                                input=b"{PSW}")
+        new_wallet = subprocess.run(["lighthouse-web3", "import-wallet", "--key", f"{PK}"],
+                                    input=b"{PSW}")
     else:
         print("Bad token")
-        wallet = None
+        new_wallet = None
 
     subprocess.run(["lighthouse-web3", "wallet"])
 
-    return wallet
+    return new_wallet
 
+
+@app.get("/get_wallet/")
+async def get_wallet():
+    wallet = subprocess.run(["lighthouse-web3", "wallet"], capture_output=True, encoding='UTF-8')
+    wallet = str(wallet.stdout).replace("\xa0", "")
+    wallet = wallet.split("\n")
+    actual_wallet = wallet[0].split(":")[1]
+    net = wallet[1].split(":")[1]
+
+    json_resp = jsonable_encoder({"Wallet": actual_wallet,
+                                  "net": net})
+
+    return JSONResponse(content=json_resp)
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8088)
