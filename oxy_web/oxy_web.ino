867ecf013d1bf53c8038a589e40d5f06fdba1607
Oriojas <oscarriojas@gmail.com>
2023-02-03 00:07:44 -0500

    fix: send data sensor

--- oxy_web/oxy_web.ino
+++ oxy_web/oxy_web.ino
@@ -1,18 +1,26 @@
-//MAX30100 ESP8266 WebServer
-#include <ESP8266WebServer.h>
-#include <Wire.h>
-#include "MAX30100_PulseOximeter.h"
 #include "env.h"
+#include <Wire.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include "MAX30100_PulseOximeter.h"
 
 
-#define REPORTING_PERIOD_MS     1000
+#define REPORTING_PERIOD_MS     4000
+
 
 float BPM, SpO2;
 
+String serverName = "http://ec2-100-24-74-70.compute-1.amazonaws.com:8088/send_data/";
+
 PulseOximeter pox;
+
 uint32_t tsLastReport = 0;
 
 ESP8266WebServer server(80);
+
+WiFiClient wifiClient;
 
 void setup() {
   Serial.begin(115200);
@@ -50,9 +58,10 @@ void setup() {
     
   }
 }
-void loop() {
-  server.handleClient();
+void loop() { 
+
   pox.update();
+  
   if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
 
     BPM = pox.getHeartRate();
@@ -64,10 +73,39 @@ void loop() {
     Serial.print("SpO2: ");
     Serial.print(int(SpO2));
     Serial.println("%");
+    
+    if (SpO2 > 0 & SpO2 < 100) {
+
+      HTTPClient http;
+      
+      String serverPath = serverName + "?user=" + user + "&bpm=" + BPM + "&spo2=" + int(SpO2); 
+           
+      http.begin(wifiClient, serverPath);                         
+      int httpCode = http.GET();      
+     
+      Serial.print("Response: ");
+      Serial.println(httpCode);
+
+      if (httpCode < 0) {
+
+          delay(5000);
+        
+        }
+        
+          
+      http.end();
+      
+      tsLastReport = millis();
+       
+    } else {
+
+        tsLastReport = millis();
+      
+      }
 
     Serial.println("*********************************");
     Serial.println();
-    tsLastReport = millis();
+    
   }
 
 
@@ -75,89 +113,9 @@ void loop() {
 
 void handle_OnConnect() {
 
-  server.send(200, "text/html", SendHTML(BPM, SpO2));
+  server.send(200, "text/plain", "OK");
 }
 
 void handle_NotFound() {
   server.send(404, "text/plain", "Not found");
-}
-
-String SendHTML(float BPM, float SpO2) {
-  String ptr = "<!DOCTYPE html>";
-  ptr += "<html>";
-  ptr += "<head>";
-  ptr += "<title>ETHeart</title>";
-  ptr += "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
-  ptr += "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css'>";
-  ptr += "<link rel='stylesheet' type='text/css' href='styles.css'>";
-  ptr += "<style>";
-  ptr += "body { background-color: #fff; font-family: sans-serif; color: #333333; font: 14px Helvetica, sans-serif box-sizing: border-box;}";
-  ptr += "#page { margin: 20px; background-color: #fff;}";
-  ptr += ".container { height: inherit; padding-bottom: 20px;}";
-  ptr += ".header { padding: 20px;}";
-  ptr += ".header h1 { padding-bottom: 0.3em; color: #008080; font-size: 45px; font-weight: bold; font-family: Garmond, 'sans-serif'; text-align: center;}";
-  ptr += "h2 { padding-bottom: 0.2em; border-bottom: 1px solid #eee; margin: 2px; text-align: left;}";
-  ptr += ".header h3 { font-weight: bold; font-family: Arial, 'sans-serif'; font-size: 17px; color: #b6b6b6; text-align: center;}";
-  ptr += ".box-full { padding: 20px; border 1px solid #ddd; border-radius: 1em 1em 1em 1em; box-shadow: 1px 7px 7px 1px rgba(0,0,0,0.4); background: #fff; margin: 20px; width: 300px;}";
-  ptr += "@media (max-width: 494px) { #page { width: inherit; margin: 5px auto; } #content { padding: 1px;} .box-full { margin: 8px 8px 12px 8px; padding: 10px; width: inherit;; float: none; } }";
-  ptr += "@media (min-width: 494px) and (max-width: 980px) { #page { width: 465px; margin 0 auto; } .box-full { width: 380px; } }";
-  ptr += "@media (min-width: 980px) { #page { width: 930px; margin: auto; } }";
-  ptr += ".sensor { margin: 12px 0px; font-size: 2.5rem;}";
-  ptr += ".sensor-labels { font-size: 1rem; vertical-align: middle; padding-bottom: 15px;}";
-  ptr += ".units { font-size: 1.2rem;}";
-  ptr += "hr { height: 1px; color: #eee; background-color: #eee; border: none;}";
-  ptr += "</style>";
-
-  //Ajax Code Start
-  ptr += "<script>\n";
-  ptr += "setInterval(loadDoc,1000);\n";
-  ptr += "function loadDoc() {\n";
-  ptr += "var xhttp = new XMLHttpRequest();\n";
-  ptr += "xhttp.onreadystatechange = function() {\n";
-  ptr += "if (this.readyState == 4 && this.status == 200) {\n";
-  ptr += "document.body.innerHTML =this.responseText}\n";
-  ptr += "};\n";
-  ptr += "xhttp.open(\"GET\", \"/\", true);\n";
-  ptr += "xhttp.send();\n";
-  ptr += "}\n";
-  ptr += "</script>\n";
-  //Ajax Code END
-
-  ptr += "</head>";
-  ptr += "<body>";
-  ptr += "<div id='page'>";
-  ptr += "<div class='header'>";
-  ptr += "<h1>ETHeart Filecoin Hack</h1>";
-  ptr += "<h3><a href='https://cyclimate'>https://cyclimate.eth</a></h3>";
-  ptr += "</div>";
-  ptr += "<div id='content' align='center'>";
-  ptr += "<div class='box-full' align='left'>";
-  ptr += "<h2>Sensor Readings</h2>";
-  ptr += "<div class='sensors-container'>";
-
-  //For Heart Rate
-  ptr += "<p class='sensor'>";
-  ptr += "<i class='fas fa-heartbeat' style='color:#cc3300'></i>";
-  ptr += "<span class='sensor-labels'> Heart Rate </span>";
-  ptr += (int)BPM;
-  ptr += "<sup class='units'>BPM</sup>";
-  ptr += "</p>";
-  ptr += "<hr>";
-
-  //For Sp02
-  ptr += "<p class='sensor'>";
-  ptr += "<i class='fas fa-burn' style='color:#f7347a'></i>";
-  ptr += "<span class='sensor-labels'> Sp02 </span>";
-  ptr += (int)SpO2;
-  ptr += "<sup class='units'>%</sup>";
-  ptr += "</p>";
-
-  ptr += "</div>";
-  ptr += "</div>";
-  ptr += "</div>";
-  ptr += "</div>";
-  ptr += "</div>";
-  ptr += "</body>";
-  ptr += "</html>";
-  return ptr;
 }
